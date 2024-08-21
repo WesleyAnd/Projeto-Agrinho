a97db7f9ead715f91d17808bbc1c939d0fe60e3a
WesleyAnd <129911678+WesleyAnd@users.noreply.github.com>
2024-08-21 00:14:48 -0300

    Add files via upload

---
+++ Códigos/animar.js
@@ -0,0 +1,44 @@
+/* cabeçalho */
+
+const menuNav = document.getElementById('botoes-mobile')
+
+menuNav.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    menuNav.classList.toggle('abrir')
+}
+
+/* conteudo principal */
+
+let count=1
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>3){
+        count=1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
+
+
+
+
+
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.terceira-parte-conteudo', {
+    rotate: { x: 0, y: 0, z: 0},
+    duration: 3000
+});
+
+sr.reveal('.segunda-parte-conteudo', {
+    rotate: { x: 0, y: 0, z: 0},
+    duration: 3000
+});
